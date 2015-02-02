912fed2a0d9f3ef4fbf536a2746e440a30890bbd
Frank Oh <kenshin579@hotmail.com>
2015-02-02 19:50:04 +0900

    added new menu

---
+++ src/osmedile/intellij/stringmanip/ToRemoveNonAlphaAndToUnderscoreAction.java
@@ -0,0 +1,13 @@
+package osmedile.intellij.stringmanip;
+
+import com.intellij.openapi.actionSystem.AnAction;
+import com.intellij.openapi.actionSystem.AnActionEvent;
+
+/**
+ * Created by ykoh on 15. 2. 2..
+ */
+public class ToRemoveNonAlphaAndToUnderscoreAction extends AnAction {
+    public void actionPerformed(AnActionEvent e) {
+        // TODO: insert action logic here
+    }
+}
