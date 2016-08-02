3b3fb52e0d891fff56b9f9b064413f0eb4444764
Vojtech Krasa <vojta.krasa@gmail.com>
2016-08-02 14:07:07 +0200

    Remove all newlines fix

--- src/osmedile/intellij/stringmanip/filter/RemoveNewLinesAction.java
+++ src/osmedile/intellij/stringmanip/filter/RemoveNewLinesAction.java
@@ -1,11 +1,22 @@
 package osmedile.intellij.stringmanip.filter;
 
+import org.apache.commons.lang.NotImplementedException;
+
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.editor.Editor;
+
 import osmedile.intellij.stringmanip.AbstractStringManipAction;
 
 public class RemoveNewLinesAction extends AbstractStringManipAction {
 
 	@Override
-	public String transformByLine(String s) {
-		return s.replaceAll("\n", "");
+	protected String transformSelection(Editor editor, DataContext dataContext, String selectedText) {
+		return selectedText.replaceAll("\n", "");
 	}
+
+	@Override
+	public String transformByLine(String s) {
+		throw new NotImplementedException();
+	}
+
 }
