9fcf4499902b3af4e29a3e0fba303f51fede432e
Vojtěch Krása <vojta.krasa@gmail.com>
2020-05-03 16:59:24 +0200

    forgotten class

---
+++ src/osmedile/intellij/stringmanip/utils/ReflectionUtils.java
@@ -0,0 +1,25 @@
+package osmedile.intellij.stringmanip.utils;
+
+import org.assertj.swing.dependency.jsr305.Nonnull;
+import org.jetbrains.annotations.Nullable;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+
+public class ReflectionUtils {
+	/**https://stackoverflow.com/a/16966699/685796*/
+	public static Iterable<Field> getFieldsUpTo(@Nonnull Class<?> startClass,
+												@Nullable Class<?> exclusiveParent) {
+
+		List<Field> currentClassFields = new ArrayList<>(List.of(startClass.getDeclaredFields()));
+		Class<?> parentClass = startClass.getSuperclass();
+
+		if (parentClass != null &&	(exclusiveParent == null || !(parentClass.equals(exclusiveParent)))) {
+			List<Field> parentClassFields =	(List<Field>) getFieldsUpTo(parentClass, exclusiveParent);
+			currentClassFields.addAll(parentClassFields);
+		}
+
+		return currentClassFields;
+	}
+}
