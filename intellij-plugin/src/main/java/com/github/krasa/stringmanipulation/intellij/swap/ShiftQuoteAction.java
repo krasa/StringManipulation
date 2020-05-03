b8fe353296d201145616003ddab2785585290260
Vojtěch Krása <vojta.krasa@gmail.com>
2020-05-03 16:59:24 +0200

    Fixes #89 Swap " and ' (not shift)

--- intellij-plugin/src/main/java/com/github/krasa/stringmanipulation/intellij/swap/ShiftQuoteAction.java
+++ intellij-plugin/src/main/java/com/github/krasa/stringmanipulation/intellij/swap/ShiftQuoteAction.java
@@ -4,20 +4,19 @@ import com.github.krasa.stringmanipulation.intellij.AbstractStringManipAction;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 
-public class SwapQuoteAction extends AbstractStringManipAction {
+public class ShiftQuoteAction extends AbstractStringManipAction {
 
 	@Override
 	protected String transformSelection(Editor editor, DataContext dataContext, String s, Object additionalParam) {
-		char[] chars = s.toCharArray();
-		for (int i = 0; i < chars.length; i++) {
-			char aChar = chars[i];
-			if (aChar == '\'') {
-				chars[i] = '\"';
-			} else if (aChar == '\"') {
-				chars[i] = '\'';
-			}
+		if (s.contains("\"")) {
+			return s.replace("\"", "'");
+		} else if (s.contains("'")) {
+			return s.replace("'", "`");
+		} else if (s.contains("`")) {
+			return s.replace("`", "\"");
+		} else {
+			return "\"" + s + "\"";
 		}
-		return String.valueOf(chars);
 	}
 
 	@Override
