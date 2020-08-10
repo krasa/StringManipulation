c2c01b2936781f2cc6fdd0e5b7e2326d9a967fd6
Vojtěch Krása <vojta.krasa@gmail.com>
2020-08-10 11:25:30 +0200

    Align multiple carets per line

--- src/osmedile/intellij/stringmanip/align/AlignCaretsAction.java
+++ src/osmedile/intellij/stringmanip/align/AlignCaretsAction.java
@@ -39,39 +39,42 @@ public class AlignCaretsAction extends MyEditorAction {
 			CaretModel caretModel = editor.getCaretModel();
 
 			SelectionSplitter.split(editor, "\n");
+			int caretIndex = 0;
+			while (caretIndex < 10_000) {
+				List<CaretState> caretsAndSelections = editor.getCaretModel().getCaretsAndSelections();
+				IdeUtils.sort(caretsAndSelections);
+				caretsAndSelections = filter2(caretIndex, caretsAndSelections);
+				Collections.reverse(caretsAndSelections);
 
-			List<CaretState> caretsAndSelections = editor.getCaretModel().getCaretsAndSelections();
-			IdeUtils.sort(caretsAndSelections);
-			caretsAndSelections = filter(caretsAndSelections, editor, caretModel);
-			Collections.reverse(caretsAndSelections);
-
-			if (caretsAndSelections.size() == 1) {
-				return;
-			}
-
-			int maxLeftColumn = caretsAndSelections.stream().mapToInt((c) -> getPositionStart(c).column).max().getAsInt();
-			int maxLength = caretsAndSelections.stream().mapToInt((c) -> getPositionEnd(c).column - getPositionStart(c).column).max().getAsInt();
-			int maxRightColumn = caretsAndSelections.stream().mapToInt((c) -> getPositionEnd(c).column).max().getAsInt();
-			maxRightColumn = Math.max(maxLength + maxLeftColumn, maxRightColumn);
-
-			for (CaretState caret : caretsAndSelections) {
-				Caret caretAt = caretModel.getCaretAt(editor.logicalToVisualPosition(caret.getCaretPosition()));
-				int pad = maxLeftColumn - getPositionStart(caret).column;
-				int padRight = maxRightColumn - (getPositionEnd(caret).column + pad);
-				int offset = editor.logicalPositionToOffset(getPositionStart(caret));
-				int offsetEnd = editor.logicalPositionToOffset(getPositionEnd(caret));
-
-				if (alignEnd) {
-					document.insertString(offsetEnd, repeat(' ', padRight));
+				if (caretsAndSelections.size() <= 1) {
+					return;
 				}
-				document.insertString(offset, repeat(' ', pad));
 
-				if (alignEnd) {
-					caretAt.setSelection(offset + pad, offsetEnd + padRight + pad);
-					caretAt.moveToOffset(offsetEnd + padRight + pad);
-				} else {
-					caretAt.moveToOffset(offset + pad);
+				int maxLeftColumn = caretsAndSelections.stream().mapToInt((c) -> getPositionStart(c).column).max().getAsInt();
+				int maxLength = caretsAndSelections.stream().mapToInt((c) -> getPositionEnd(c).column - getPositionStart(c).column).max().getAsInt();
+				int maxRightColumn = caretsAndSelections.stream().mapToInt((c) -> getPositionEnd(c).column).max().getAsInt();
+				maxRightColumn = Math.max(maxLength + maxLeftColumn, maxRightColumn);
+
+				for (CaretState caret : caretsAndSelections) {
+					Caret caretAt = caretModel.getCaretAt(editor.logicalToVisualPosition(caret.getCaretPosition()));
+					int pad = maxLeftColumn - getPositionStart(caret).column;
+					int padRight = maxRightColumn - (getPositionEnd(caret).column + pad);
+					int offset = editor.logicalPositionToOffset(getPositionStart(caret));
+					int offsetEnd = editor.logicalPositionToOffset(getPositionEnd(caret));
+
+					if (alignEnd) {
+						document.insertString(offsetEnd, repeat(' ', padRight));
+					}
+					document.insertString(offset, repeat(' ', pad));
+
+					if (alignEnd) {
+						caretAt.setSelection(offset + pad, offsetEnd + padRight + pad);
+						caretAt.moveToOffset(offsetEnd + padRight + pad);
+					} else {
+						caretAt.moveToOffset(offset + pad);
+					}
 				}
+				caretIndex++;
 			}
 		}
 
@@ -85,6 +88,24 @@ public class AlignCaretsAction extends MyEditorAction {
 			throw new UnsupportedOperationException();
 		}
 
+		private List<CaretState> filter2(int caretIndex, List<CaretState> caretsAndSelections) {
+			int previousLine = -1;
+			int index = 0;
+			List<CaretState> caretStates = new ArrayList<>();
+			for (CaretState caret : caretsAndSelections) {
+				int line = getPositionStart(caret).line;
+				if (getPositionStart(caret).line == previousLine) {
+					index++;
+				} else {
+					index = 0;
+				}
+				if (index == caretIndex) {
+					caretStates.add(caret);
+				}
+				previousLine = line;
+			}
+			return caretStates;
+		}
 
 		private List<CaretState> filter(List<CaretState> caretsAndSelections, Editor editor, CaretModel caretModel) {
 			int previousLine = -1;
