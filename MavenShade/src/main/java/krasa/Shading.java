5bacaabb217bf758a3cc6968fd2462afe12392d3
Vojtěch Krása <vojta.krasa@gmail.com>
2021-01-07 09:55:19 +0100

    #117 Minify JavaScript

--- MavenShade/src/main/java/krasa/Shading.java
+++ MavenShade/src/main/java/krasa/Shading.java
@@ -1,9 +1,12 @@
 package krasa;
 
+import ro.isdc.wro.model.resource.processor.support.JSMin;
+
 import java.io.UnsupportedEncodingException;
 
 public class Shading {
     public static void main(String[] args) throws UnsupportedEncodingException {
         org.springframework.web.util.UriUtils.encode("ffoo", "UTF-8");
+        Class<JSMin> jsMinClass = JSMin.class;
     }
 }
---
+++ src/osmedile/intellij/stringmanip/transform/MinifyJavaScriptAction.java
@@ -0,0 +1,38 @@
+package osmedile.intellij.stringmanip.transform;
+
+import com.intellij.openapi.actionSystem.DataContext;
+import com.intellij.openapi.diagnostic.Logger;
+import com.intellij.openapi.editor.Editor;
+import com.intellij.openapi.progress.ProcessCanceledException;
+import com.intellij.openapi.ui.Messages;
+import org.apache.commons.lang.CharEncoding;
+import osmedile.intellij.stringmanip.AbstractStringManipAction;
+import ro.isdc.wro.model.resource.processor.support.JSMin;
+
+import javax.swing.*;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.nio.charset.StandardCharsets;
+import java.util.Map;
+
+public class MinifyJavaScriptAction extends AbstractStringManipAction<Object> {
+	private static final Logger LOG = Logger.getInstance(MinifyJavaScriptAction.class);
+
+	@Override
+	protected String transformSelection(Editor editor, Map<String, Object> actionContext, DataContext dataContext, String selectedText, Object additionalParam) {
+		try {
+			ByteArrayOutputStream out = new ByteArrayOutputStream();
+			new JSMin(new ByteArrayInputStream(selectedText.getBytes(StandardCharsets.UTF_8)), out).jsmin();
+			return out.toString(CharEncoding.UTF_8).trim();
+		} catch (Throwable e) {
+			SwingUtilities.invokeLater(() -> Messages.showErrorDialog(editor.getProject(), String.valueOf(e), "Minify JavaScript"));
+			LOG.info(e);
+			throw new ProcessCanceledException(e);
+		}
+	}
+
+	@Override
+	public String transformByLine(Map<String, Object> actionContext, String s) {
+		throw new RuntimeException();
+	}
+}
