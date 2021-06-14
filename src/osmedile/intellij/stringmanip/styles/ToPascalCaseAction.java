88f8d52a62601eab18afa8a3b33895d3c1226796
Vojtěch Krása <vojta.krasa@gmail.com>
2021-06-14 12:55:30 +0200

    Fixes #144 ToPascalCaseAction

--- src/osmedile/intellij/stringmanip/styles/ToPascalCaseAction.java
+++ src/osmedile/intellij/stringmanip/styles/ToPascalCaseAction.java
@@ -12,12 +12,7 @@ public class ToPascalCaseAction extends AbstractCaseConvertingAction {
 
 	@Override
 	public String transformByLine(Map<String, Object> actionContext, String s) {
-        Style from = getStyle(actionContext, s);
-        if (from == Style.PASCAL_CASE || from == Style._SINGLE_WORD_CAPITALIZED) {
-            return Style.CAMEL_CASE.transform( s);
-        } else {
-            return Style.PASCAL_CASE.transform( s);
-        }
+        return Style.PASCAL_CASE.transform(s);
     }
 
 }
