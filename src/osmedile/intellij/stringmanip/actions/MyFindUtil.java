02a01a7cd590403876777a80c132b532c3286954
Vojtěch Krása <vojta.krasa@gmail.com>
2021-12-19 09:52:23 +0100

    license

--- src/osmedile/intellij/stringmanip/actions/MyFindUtil.java
+++ src/osmedile/intellij/stringmanip/actions/MyFindUtil.java
@@ -1,3 +1,6 @@
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+// Copyright 2000-2021 JetBrains s.r.o. Use of this source code is governed by the Apache 2.0 license that can be found in the LICENSE file.
+
 package osmedile.intellij.stringmanip.actions;
 
 import com.intellij.find.FindBundle;
