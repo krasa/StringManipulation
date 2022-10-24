146040ac85aa25ac65d6dcec2c8b56ddb80ccada
Vojtěch Krása <vojta.krasa@gmail.com>
2022-10-24 21:00:05 +0200

    fix for: UnsupportedOperationException: TextComponentEditorAction is updated on EDT only and must not work in injected context according to: update SelectWordAtCaretAction action in BGT 21.10.2022 23:14 Gregory.Shrago*

--- src/osmedile/intellij/stringmanip/actions/SelectWordInDifferentHumpsModeAction.java
+++ src/osmedile/intellij/stringmanip/actions/SelectWordInDifferentHumpsModeAction.java
@@ -1,16 +1,18 @@
 package osmedile.intellij.stringmanip.actions;
 
 import com.intellij.openapi.actionSystem.ActionManager;
+import com.intellij.openapi.actionSystem.ActionUpdateThread;
 import com.intellij.openapi.actionSystem.DataContext;
 import com.intellij.openapi.editor.Editor;
 import com.intellij.openapi.editor.EditorSettings;
 import com.intellij.openapi.editor.actionSystem.EditorAction;
 import com.intellij.openapi.editor.actions.TextComponentEditorAction;
 import com.intellij.openapi.project.DumbAware;
+import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 import osmedile.intellij.stringmanip.MyEditorWriteActionHandler;
 
-public class SelectWordInDifferentHumpsModeAction extends TextComponentEditorAction implements DumbAware {
+public class SelectWordInDifferentHumpsModeAction extends EditorAction implements DumbAware {
 
 	protected SelectWordInDifferentHumpsModeAction() {
 		super(new MyEditorWriteActionHandler<String>(SelectWordInDifferentHumpsModeAction.class) {
@@ -34,4 +36,13 @@ public class SelectWordInDifferentHumpsModeAction extends TextComponentEditorAct
 		setInjectedContext(true);
 	}
 
+	@Override
+	public final @NotNull ActionUpdateThread getActionUpdateThread() {
+		return ActionUpdateThread.BGT;
+	}
+
+	@Override
+	protected @Nullable Editor getEditor(@NotNull DataContext dataContext) {
+		return TextComponentEditorAction.getEditorFromContext(dataContext);
+	}
 }
